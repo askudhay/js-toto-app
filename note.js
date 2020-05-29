9935a743f570fe399d12ee2720eb2b0099f0e317
askudhay <systemudhaya@gmail.com>
2020-05-29 15:01:34 -0400

    added peacock and prettier

--- note.js
+++ note.js
@@ -1,17 +1,16 @@
-function addTask(){
-   let taskIPObj = document.getElementById('taskInput');  
-   let task = taskIPObj.value;
+function addTask() {
+  let taskIPObj = document.getElementById("taskInput");
+  let task = taskIPObj.value;
 
-   if(task.trim() == ''){
-      alert('Please enter Task before you try to add!');
-      return;
-   }
+  if (task.trim() == "") {
+    alert("Please enter Task before you try to add!");
+    return;
+  }
 
-   let textNode = document.createTextNode(task);
-   let pNode = document.createElement('p');
+  let textNode = document.createTextNode(task);
+  let pNode = document.createElement("p");
 
-   pNode.appendChild(textNode);
-   document.getElementById("tasks").appendChild(pNode);
-   taskIPObj.value = '';
-
+  pNode.appendChild(textNode);
+  document.getElementById("tasks").appendChild(pNode);
+  taskIPObj.value = "";
 }
